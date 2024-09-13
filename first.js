16744a38c10042d7f928448df4461295308e9269
Khushiarzare <147649983+Khushiarzare@users.noreply.github.com>
2024-09-13 16:28:40 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,20 @@
+
+var total = 2000;
+
+function checkpin()
+ {
+  let input=document.getElementById('result1').value
+  if (input=='1428') 
+    {
+    document.getElementById('done').innerHTML="DONE";
+    } 
+    else 
+   {
+    document.getElementById('done').innerHTML="WRONG PIN";
+   }
+}
+function withdraw()
+{
+ let amount=document.getElementById()
+
+}
